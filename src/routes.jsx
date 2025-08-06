69ea95ccfb554a4bbd99e369932fb0707e0bbaed
Nikhil-keshri2213 <nikhilkeshri2213@gmail.com>
2025-08-07 01:32:44 +0530

    Uploading ongoing project detail to track, current work SizeFilter

---
+++ src/routes.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from './Layout';
+import App from './App'; // Homepage
+import { ProductListPage } from "./pages/productListPage/ProductListPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+      {
+        path: "mens",
+        element: <ProductListPage categoryType={'MEN'} />,
+      },
+      {
+        path: "womens",
+        element: <ProductListPage categoryType={'WOMEN'}/>,
+      },
+      {
+        path: "kids",
+        element: <ProductListPage categoryType={'KIDS'}/>,
+      },
+    ],
+  },
+]);
