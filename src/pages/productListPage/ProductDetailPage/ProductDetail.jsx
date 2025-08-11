9e56452d0793d6b090fda5d56920fa9c62d81c25
Nikhil-keshri2213 <nikhilkeshri2213@gmail.com>
2025-08-12 01:35:05 +0530

    Product Detailed Page 90% Done and various updation done

--- src/pages/productListPage/ProductDetailPage/ProductDetail.jsx
+++ src/pages/productListPage/ProductDetailPage/ProductDetail.jsx
@@ -6,20 +6,49 @@ import Rating from "../../../components/Rating/rating";
 import {SizeFilter} from '../../../components/Filters/SizeFilter'
 import ProductColors from "./ProductColors";
 import {CartIcon} from '../../../components/Common/CartIcon'
-
+import SvgCreditCard from '../../../components/Common/SvgCreditCard'
+import SvgCloth from '../../../components/Common/SvgCloth'
+import SvgShipping from '../../../components/Common/SvgShipping'
+import SvgReturn from '../../../components/Common/SvgReturn'
+import SectionHeading from "../../../components/Sections/SectionHeading/SectionHeading";
+import ProductCard from "../ProductCard";
 
 const categories = content?.categories;
 
+const extraSections = [
+  {
+    icon:<SvgCreditCard/>,
+    label: 'Secure Payments'
+  },
+  {
+    icon:<SvgCloth />,
+    label:'Size & Fit'
+  },
+  {
+    icon:<SvgShipping />,
+    label:'Free Shipping'
+  },
+  {
+    icon:<SvgReturn />,
+    label:'Return Policy is Available'
+  }
+]
+
 export const ProductDetail = () => {
   const { product } = useLoaderData();
-  const [image, setImage] = useState(product?.images[0]?.startsWith('http') ? product.images[0] : product?.thumbnail);
+  const [image, setImage] = useState();
   const [BreadcrumbLink, setBreadcrumbLink] = useState([]);
-
+  
+  const similarProduct = useMemo(()=>{
+    return content?.products?.filter((item)=> item?.type_id === product?.type_id && item?.id !== product?.id)
+  },[product]);
+  
   const productCategory = useMemo(()=>{
     return categories?.find((category)=> category?.id === product?.category_id);
   },[product]);
 
   useEffect(()=>{
+    setImage(product?.images[0]?.startsWith('http') ? product.images[0] : product?.thumbnail)
     setBreadcrumbLink([]);
     const arrayLinks = [{title:'Shop',path:'/'},{
       title:productCategory?.name,
@@ -27,8 +56,7 @@ export const ProductDetail = () => {
     }];
     
     const productType = productCategory?.types?.find((item)=>item?.type_id === product?.type_id);
-    console.log("Product Type",productType, productCategory);
-    
+
     if(productType){
       arrayLinks?.push({
       title:productType?.name,
@@ -38,8 +66,10 @@ export const ProductDetail = () => {
   },[productCategory, product])
 
   return (
+    <>
     <div className="flex flex-col md:flex-row p-10">
       <div className="w-[100%] lg:w-[50%] md:w-[40%]">
+        
         {/* Image */}
         <div className="flex flex-col md:flex-row">
           <div className="w-[100%] md:w-[30%] justify-center h-[40px] md:h-[420px]">
@@ -57,6 +87,7 @@ export const ProductDetail = () => {
               }
             </div>
           </div>
+          
           <div className="w-full md:w-[80%] flex justify-center md:pt-0 pt-10">
             <img
               src={image}
@@ -64,29 +95,76 @@ export const ProductDetail = () => {
               className="h-full w-full max-h-[520px] border rounded-lg cursor-pointer object-cover"
             />
           </div>
+
         </div>
       </div>
+
       <div className="w-[60%] px-10">
         {/* Product Description */}
         <Breadcrumb links={BreadcrumbLink}/>
+        
         <p className="text-3xl pt-3 pb-4">{product?.title}</p>
-        <Rating rating={product?.rating}/> 
+        
+        <Rating rating={product?.rating}/>
+        
+        {/* Price Tag */}
+        <p className="text-xl bold pt-2 px-2">${product?.price}</p>
+        
         <div className="flex flex-col pt-3">
           <div className="flex gap-2">
             <p className="text-sm bold px-2">Select Size</p>
             <Link className="text-sm text-gray-500 hover:text-gray-900" to={'https://en.wikipedia.org/wiki/Clothing_sizes'} target="blank">{'Size Guide ->'}</Link>
           </div>
         </div>
-        <div className="mt-3"><SizeFilter sizes={product?.size} hideTitle={true}/></div>
+        
+        <div className="mt-3">
+          <SizeFilter sizes={product?.size} hideTitle={true}/>
+        </div>
+        
         <div>
           <p className="text-sm px-2">Colors Available</p>
           <ProductColors colors={product?.color}/>
         </div>
-        <div className="flex pt-5 px-2">
+        
+        <div className="flex pt-5 px-2 pb-4">
           <button className="flex items-center gap-2 bg-black text-white rounded-lg px-4 py-2"><CartIcon bgColor="black" />Add to cart</button>
         </div>
+
+        <hr />
+        
+        <div className="grid grid-cols-2 pt-4 gap-4 text-gray-600">
+          {
+            extraSections?.map((section)=>(
+            <div className="flex items-center">
+                {section?.icon}
+                <p className="px-3">{section?.label}</p>
+              </div>
+            ))
+          }
+        </div>
+
       </div>
     </div>
+
+    {/* Product Description */}
+    <SectionHeading title={"| Product Description"}/>
+    <div className="md:w-[50%] w-full px-2 pb-5">
+      <p className="px-14">{product?.description}</p>
+    </div>
+
+    {/* Similar Product */}
+    <SectionHeading title={"| Similar Product"}/>
+    <div className="flex px-10">
+      <div className="pt-4 px-12 pb-4 grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-4">
+        {/* <ProductCard {...productListItems[0]}/> */}
+        {
+          similarProduct?.map((item, index) => {
+          return <ProductCard key={index} {...item} />;})
+        }
+        {!similarProduct?.length && <p>More Products Comming soon.</p> }
+      </div>
+    </div>
+    </>
   );
 };
 export default ProductDetail
--- src/pages/productListPage/ProductListPage.jsx
+++ src/pages/productListPage/ProductListPage.jsx
@@ -1,57 +1,58 @@
-import React, { useMemo } from 'react'
-import FilterIcon from '../../components/Common/FilterIcon'
-import content from '../../data/content.json'
-import { Categories } from '../../components/Filters/Categories'
-import { PriceFilter } from '../../components/Filters/PriceFilter'
-import { ColorFilter } from '../../components/Filters/ColorFilter'
-import { SizeFilter } from '../../components/Filters/SizeFilter'
-import { ProductCard } from './ProductCard'
+import React, { useMemo } from "react";
+import FilterIcon from "../../components/Common/FilterIcon";
+import content from "../../data/content.json";
+import { Categories } from "../../components/Filters/Categories";
+import { PriceFilter } from "../../components/Filters/PriceFilter";
+import { ColorFilter } from "../../components/Filters/ColorFilter";
+import { SizeFilter } from "../../components/Filters/SizeFilter";
+import { ProductCard } from "./ProductCard";
 
-const categories = content?.categories
+const categories = content?.categories;
 
-export const ProductListPage = ({categoryType}) => {
-  
-  const categoryContent = useMemo(() =>{
+export const ProductListPage = ({ categoryType }) => {
+  const categoryContent = useMemo(() => {
     return categories?.find((category) => category.code === categoryType);
-  },[categoryType]) 
-  
-  const productListItems = useMemo(()=>{
-    return content?.products?.filter((product)=>product?.category_id === categoryContent?.id);
-  },[categoryContent])
+  }, [categoryType]);
+
+  const productListItems = useMemo(() => {
+    return content?.products?.filter(
+      (product) => product?.category_id === categoryContent?.id
+    );
+  }, [categoryContent]);
 
   return (
-      <div className='flex'>
-        <div className='w-[20%] p-[20px] border rounded-lg m-[20px]'>
-          <div className='flex justify-between'>
-            <p className='text-[16px] text-gray-600'>Filter</p>
-            <FilterIcon />
-          </div>
-          <div>
-            <p className='text-[16px] text-black mt-5'>Categories</p>
-            <Categories types={categoryContent?.types}/>
-          </div>
-          <hr></hr>
-          {/* Price Filter */}
-          <PriceFilter/>
-          <hr></hr>
-          {/* Color Filter */}
-          <ColorFilter colors={categoryContent?.meta_data?.colors}/>
-          <hr></hr>
-          {/* Size Filter */}
-          <SizeFilter sizes={categoryContent?.meta_data?.sizes}/>
+    <div className="flex">
+      <div className="w-[20%] p-[20px] border rounded-lg m-[20px]">
+        <div className="flex justify-between">
+          <p className="text-[16px] text-gray-600">Filter</p>
+          <FilterIcon />
         </div>
+        <div>
+          <p className="text-[16px] text-black mt-5">Categories</p>
+          <Categories types={categoryContent?.types} />
+        </div>
+        <hr></hr>
+        {/* Price Filter */}
+        <PriceFilter />
+        <hr></hr>
+        {/* Color Filter */}
+        <ColorFilter colors={categoryContent?.meta_data?.colors} />
+        <hr></hr>
+        {/* Size Filter */}
+        <SizeFilter sizes={categoryContent?.meta_data?.sizes} />
+      </div>
 
-        <div className='p-[20px]'>
-          <p className='text-black text-lg'>{categoryContent?.description}</p>
-          {/* Products */}
-          <div className='pt-4 px-2 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-8'>
-            {/* <ProductCard {...productListItems[0]}/> */}
-            
-            {productListItems?.map((item, index)=>{
-              return <ProductCard key={index} {...item}/>
-            })}
-          </div>
+      <div className="p-[20px]">
+        <p className="text-black text-lg">{categoryContent?.description}</p>
+        {/* Products */}
+
+        <div className="pt-4 px-2 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-8">
+          {/* <ProductCard {...productListItems[0]}/> */}
+          {productListItems?.map((item, index) => {
+            return <ProductCard key={index} {...item} />;
+          })}
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
