71717323d5fe7c5932ae6480aeaec440a4dfd47f
Nikhil-keshri2213 <nikhilkeshri2213@gmail.com>
2025-12-06 23:01:50 +0530

    Adding API link

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,29 +1,3 @@
-// import React, { useMemo } from 'react';
-// import SvgStarIcon from '../Common/SvgStarIcon.jsx';
-// import SvgEmptyStar from '../Common/SvgEmptyStar.jsx'
-
-// const Rating = ({ rating }) => {
-//   const ratingNumber = useMemo(() => {
-//     return Array(Math.round(Number(rating))).fill(null);
-//   }, [rating]);
-
-//   return (
-//     <div className='flex items-center'>
-//       {ratingNumber.map((_, index) => (
-//         <SvgStarIcon key={index} />
-//       ))}
-//       {
-//         Array(5-ratingNumber?.length).fill().map((_,index)=>(
-//           <SvgEmptyStar key={'empty-'+index}/>
-//         ))
-//       }
-//       <p className='px-2 text-gray-500'>{rating}</p>
-//     </div>
-//   );
-// };
-
-// export default Rating;
-
 import React, { useMemo } from "react";
 import SvgStarIcon from "../Common/SvgStarIcon.jsx";
 import SvgEmptyStar from "../Common/SvgEmptyStar.jsx";
